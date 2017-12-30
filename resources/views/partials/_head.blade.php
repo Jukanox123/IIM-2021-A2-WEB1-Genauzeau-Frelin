09e642a3c38589f001cbff21b3deff031f47c1e9
Valentin-Genauzeau <vgenauzeau@gmail.com>
2017-12-30 20:30:43 +0100

    fourteenth commit (Adding Features to BlogController)

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -7,16 +7,17 @@
 
 <title>Blog @yield('title')</title>
 
-<!-- Bootstrap core CSS -->
+<!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
       integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+      integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 {{ Html::style('css/styles.css') }}
 
 @yield('stylesheets')
 
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]>
-<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,6 +1,7 @@
 <!-- Static navbar -->
 <nav class="navbar navbar-default">
     <div class="container-fluid">
+
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
                     aria-expanded="false" aria-controls="navbar">
@@ -11,12 +12,29 @@
             </button>
             <a class="navbar-brand" href="#">Laravel Blog</a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
+
+        <div id="bs-example-navbar-collapse-1" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li class="{{ Request::is('/') ? "active" : "" }}"><a href="/">Home</a></li>
+                <li class="{{ Request::is('blog') ? "active" : "" }}"><a href="/blog">Blog</a></li>
+                <li class="{{ Request::is('about') ? "active" : "" }}"><a href="/about">About</a></li>
+                <li class="{{ Request::is('contact') ? "active" : "" }}"><a href="/contact">Contact</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                       aria-expanded="false">My Account <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="{{ route('posts.index') }}">Posts</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Logout</a></li>
+                    </ul>
+                </li>
             </ul>
         </div><!--/.nav-collapse -->
+
     </div><!--/.container-fluid -->
 </nav>
